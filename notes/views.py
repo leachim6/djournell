58ae527510bf89b2acbf0510926532c042a8eb90
Mike Donaghy <mike@mikedonaghy.org>
2007-09-15 19:45:20 -0400

    Fixed all errors , you can now view notes by course , by going to <url>/course/course_id

--- notes/views.py
+++ notes/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.views.generic.list_detail import object_list
+from cornell.notes.models import *
+
+def notes_for_course(request, course_id):
+    course = Course.objects.get(id=course_id)
+    notes  = Course.note_set.all()
+    return object_list(request, queryset=notes,template_name='Notes/notes_for_course.html',is_paginated=True,paginate_by=15)
--- urls.py
+++ urls.py
@@ -13,5 +13,6 @@ urlpatterns = patterns('',
         (r'^admin/', include('django.contrib.admin.urls')),
         (r'^$', 'django.views.generic.list_detail.object_list', info_dict),
         (r'^note/(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', detail_dict),
-        (r'^public/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'f:/media/code/python/cornell/public'})
+        (r'^public/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'f:/media/code/python/cornell/public'}),
+        (r'^course/(?P<course_id>\d+)/$', 'cornell.notes.views.notes_for_course'),
 )
