168a7bc6d5ad67a25c291ce851bed2ffded2f208
Mike Donaghy <mike@mikedonaghy.org>
2007-09-15 15:50:44 -0400

    fixed error in model file

--- notes/models.py
+++ notes/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
+class Courses(models.Model):
+    name = models.CharField(max_length=40)
+
 class Note(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=40)
     course = models.ManyToManyField(Courses)
     keycol = models.TextField()
     bodycol = models.TextField()
     pub_date = models.DateTimeField()
-    comments_enabled = models.BooleanField
-
-class Courses(models.Model):
-    name = models.CharField()
+    comments_enabled = models.BooleanField()
