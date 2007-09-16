2f3a50aca4838f687b176b308640dfc35333e92b
Mike Donaghy <mike@mikedonaghy.org>
2007-09-15 22:32:19 -0400

    Cleaned up home-page html quite a bit

--- settings.py
+++ settings.py
@@ -83,3 +83,11 @@ INSTALLED_APPS = (
     'django.contrib.comments',
     'django.contrib.markup'
 )
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.request",
+)
